40fa41bf9195ddc7dc7bc6dfc35663583d97a3b0
Jacob Quant <jacobq@gmail.com>
2020-10-08 09:05:18 -0500

    Initial commit: basic examples

---
+++ main.c
@@ -0,0 +1,100 @@
+#include <stdio.h>
+#include <math.h>
+
+#include <gsl/gsl_errno.h>
+#include <gsl/gsl_rstat.h>
+
+char global_abort = 0;
+gsl_rstat_workspace *global_rstat_p = NULL;
+
+int add(double x, gsl_rstat_workspace *rstat_p) {
+  global_rstat_p = rstat_p;
+  int retval = gsl_rstat_add(x, rstat_p);
+  if (retval) {
+    printf("gsl_rstat_add returned %d\n", retval);
+    fflush(stdout);
+  }
+  return retval;
+}
+
+void handler(const char * reason, const char * file, int line, int gsl_errno) {
+  global_abort = 1;
+  printf("Reason: %s, File: %s, Line: %d, gsl_errno: %u\n", reason, file, line, gsl_errno);
+  if (global_rstat_p) {
+    gsl_rstat_workspace *p1 = global_rstat_p;
+    printf("min=%.6e, max=%.6e, mean=%.6e, M2=%.6e, M3=%.6e, M4=%.6e, n=%lu\n",
+      p1->min, p1->max, p1->mean, p1->M2, p1->M3, p1->M4, p1->n
+    );
+
+    gsl_rstat_quantile_workspace *p2 = p1->median_workspace_p;
+    printf("p=%.6e, n=%lu\n", p2->p, p2->n);
+    // q[0], p->q[1], p->q[2], p->q[3], p->q[4]
+    for (size_t i=0; i < 5; i++) {
+      printf("[%lu] q=%.6e, npos=%d, np=%.6e, dnp=%.6e\n",
+        i, p2->q[i], p2->npos[i], p2->np[i], p2->dnp[i]
+      );
+    }
+    fflush(stdout);
+  }
+}
+
+
+void death1() {
+  gsl_rstat_workspace *rstat_p = gsl_rstat_alloc();
+  gsl_rstat_add(-INFINITY, rstat_p);
+  gsl_rstat_add(+INFINITY, rstat_p);
+  gsl_rstat_add(-INFINITY, rstat_p);
+  gsl_rstat_add(+INFINITY, rstat_p);
+  gsl_rstat_add(-INFINITY, rstat_p);
+  gsl_rstat_add(+INFINITY, rstat_p);
+  gsl_rstat_add(-INFINITY, rstat_p);
+  gsl_rstat_add(+INFINITY, rstat_p);
+  printf("n=%lu\n", gsl_rstat_n(rstat_p));
+  printf("Goodbye, cruel world!\n");
+  fflush(stdout);
+  //gsl_rstat_add(-INFINITY, rstat_p);
+  add(-INFINITY, rstat_p);
+  gsl_rstat_free(rstat_p);
+}
+
+
+void death2() {
+  gsl_rstat_workspace *rstat_p = gsl_rstat_alloc();
+  global_abort = 0;
+  const size_t N = 16 * 1024 * 1024;
+  const double xs[] = {
+    1e308, 1.2e308, 1.5e308, 1.6e308, 1.7e308, 1.7e+308,
+    1e308, 1.2e308, 1.5e308, 1.6e308, 1.7e308, 1.7e+308,
+    -1e308, -1.2e308, -1.5e308, -1.6e308, -1.7e308, -1.7e+308,
+    //8.99E+307, -8.99E+307,
+    //(double)(18428729675200000000UL - 18328729675200000000UL)
+    // double range is 2.3E-308 to 1.7E+308
+    //HUGE_VALF, -HUGE_VALF,
+    //-INFINITY, INFINITY,
+    //0, -1e9, +1e9, -5e100, +5e100,
+  };
+  const size_t xs_len = sizeof(xs)/sizeof(xs[0]);
+  size_t j = 0;
+  for (size_t i = 0; i < N; ++i) {
+    if (i < 25 || i % 8192 == 0) {
+      printf("%lu %.8e %.8e\n", i+1, xs[j], gsl_rstat_mean(rstat_p));
+    }
+    //gsl_rstat_add(xs[j], rstat_p);
+    add(xs[j], rstat_p);
+    if (global_abort) {
+      printf("Stopping at i=%lu since error handler set abort flag.\n", i);
+    }
+    if (++j >= xs_len) {
+      j = 0;
+    }
+  }
+  gsl_rstat_free(rstat_p);
+}
+
+int main()
+{
+  gsl_set_error_handler(&handler);
+  death1();
+  death2();
+  return 0;
+}
